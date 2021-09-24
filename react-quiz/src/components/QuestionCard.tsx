60fb2dba17da7f5170a68b69585c1e6135d7d61f
Sepehr Sobhani <sobhani.sepehr@gmail.com>
2021-09-24 00:12:10 -0700

    Create question card styles

--- react-quiz/src/components/QuestionCard.tsx
+++ react-quiz/src/components/QuestionCard.tsx
@@ -1,32 +1,38 @@
 import React from "react";
+// Types
+import { AnswerObject } from "../App";
+// Styles
+import { Wrapper, ButtonWrapper } from "./QuestionCard.styles";
 
 type Props = {
-    question: string;
-    answers: string[];
-    callback: any;
-    userAnswer: any;
-    questionNr: number;
-    totalQuestions: number;
-}
+  question: string;
+  answers: string[];
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
+  questionNr: number;
+  totalQuestions: number;
+};
 
-const QuestionCard: React.FC<Props> = ({question, answers, callback, userAnswer, questionNr, totalQuestions}) => 
-(
-<div>
+const QuestionCard: React.FC<Props> = ({ question, answers, callback, userAnswer, questionNr, totalQuestions }) => (
+  <Wrapper>
     <p className="number">
-        Question: {questionNr} / {totalQuestions}
+      Question: {questionNr} / {totalQuestions}
     </p>
-    <p dangerouslySetInnerHTML={{__html: question}} />
+    <p dangerouslySetInnerHTML={{ __html: question }} />
     <div>
-        {answers.map(answer => (
-            <div key={answer}>
-                <button disabled={userAnswer} value={answer} onClick={callback}>
-                    <span dangerouslySetInnerHTML={{__html: answer}} />
-                </button>
-            </div>
-        ))}
+      {answers.map((answer) => (
+        <ButtonWrapper
+          correct={userAnswer?.correctAnswer === answer}
+          userClicked={userAnswer?.answer === answer}
+          key={answer}
+        >
+          <button disabled={!!userAnswer} value={answer} onClick={callback}>
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
+          </button>
+        </ButtonWrapper>
+      ))}
     </div>
-</div>
+  </Wrapper>
 );
-
 
 export default QuestionCard;
